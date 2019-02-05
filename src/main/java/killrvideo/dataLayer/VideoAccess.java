4d09968ed6b6aebbdb21700611ee82b740f1f59c
Jamie King <jamie.king@datastax.com>
2019-02-05 22:08:18 +0000

    Adding imports to VideoAccess

--- src/main/java/killrvideo/dataLayer/VideoAccess.java
+++ src/main/java/killrvideo/dataLayer/VideoAccess.java
@@ -1,5 +1,16 @@
 package killrvideo.dataLayer;
 
+import java.io.File;
+import java.time.Instant;
+
+import com.datastax.dse.driver.api.core.DseSession;
+import com.datastax.oss.driver.api.core.DefaultConsistencyLevel;
+import com.datastax.oss.driver.api.core.cql.BoundStatement;
+import com.datastax.oss.driver.api.core.cql.PreparedStatement;
+import com.datastax.oss.driver.api.core.cql.SimpleStatement;
+import com.datastax.oss.driver.api.querybuilder.QueryBuilderDsl;
+import com.datastax.oss.driver.api.querybuilder.insert.RegularInsert;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 

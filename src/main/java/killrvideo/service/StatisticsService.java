0a47ccbc8d75794188f59b10e3f8c229efc52998
Jamie King <jamie.king@datastax.com>
2019-04-09 22:51:23 +0000

    Adding video statistics so latest videos show up on the homepage.

--- src/main/java/killrvideo/service/StatisticsService.java
+++ src/main/java/killrvideo/service/StatisticsService.java
@@ -7,9 +7,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import io.grpc.stub.StreamObserver;
+import killrvideo.common.CommonTypes.Uuid;
 import killrvideo.statistics.StatisticsServiceGrpc.StatisticsServiceImplBase;
 import killrvideo.statistics.StatisticsServiceOuterClass.GetNumberOfPlaysRequest;
 import killrvideo.statistics.StatisticsServiceOuterClass.GetNumberOfPlaysResponse;
+import killrvideo.statistics.StatisticsServiceOuterClass.PlayStats;
 import killrvideo.statistics.StatisticsServiceOuterClass.RecordPlaybackStartedRequest;
 import killrvideo.statistics.StatisticsServiceOuterClass.RecordPlaybackStartedResponse;
 
@@ -31,6 +33,18 @@ public class StatisticsService extends StatisticsServiceImplBase {
     public void getNumberOfPlays(GetNumberOfPlaysRequest request, StreamObserver<GetNumberOfPlaysResponse> responseObserver) {
 
         LOGGER.debug("-----Start getting number of plays------");
+        final GetNumberOfPlaysResponse.Builder builder = GetNumberOfPlaysResponse.newBuilder();
+                
 
+        for (Uuid requestedVideoId : request.getVideoIdsList()) {
+            builder.addStats(PlayStats
+                .newBuilder()
+                .setVideoId(requestedVideoId)
+                .setViews(0L)
+                .build());
+        }
+                                        
+        responseObserver.onNext(builder.build());
+        responseObserver.onCompleted();
     }
 }

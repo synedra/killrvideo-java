9cfad715fafcb1587eccafb1e844f50172db9938
Jamie King <mister.net@gmail.com>
2019-04-22 13:44:45 -0600

    Updating imports

--- src/main/java/killrvideo/dataLayer/UserAccess.java
+++ src/main/java/killrvideo/dataLayer/UserAccess.java
@@ -4,13 +4,19 @@ import java.io.File;
 import java.util.UUID;
 import java.time.Instant;
 
+import com.datastax.oss.driver.api.core.cql.BatchStatement;
+import com.datastax.oss.driver.api.core.cql.BoundStatement;
+import com.datastax.oss.driver.api.core.cql.DefaultBatchType;
+import com.datastax.oss.driver.api.core.cql.PreparedStatement;
+import com.datastax.oss.driver.api.core.cql.SimpleStatement;
+import com.datastax.oss.driver.api.core.cql.SimpleStatementBuilder;
+import com.datastax.dse.driver.api.core.DseSession;
+import com.datastax.oss.driver.api.core.DefaultConsistencyLevel;
+import com.datastax.oss.driver.api.querybuilder.QueryBuilder;
+import com.datastax.oss.driver.api.querybuilder.insert.RegularInsert;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.datastax.dse.driver.api.core.DseSession;
-import com.datastax.oss.driver.api.core.cql.ResultSet;
-import com.datastax.oss.driver.api.core.cql.Row;
-import com.datastax.oss.driver.api.core.cql.SimpleStatement;
 
 import killrvideo.entity.User;
 
